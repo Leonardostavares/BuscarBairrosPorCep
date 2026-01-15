09485e045b99918bfdcf9bf9ce7d411bb8fcff66
Leonardostavares <leosilvatavares13@gmail.com>
2026-01-15 09:32:49 -0300

    Adicionando Cache na aplicacao pafra diminuir requisicoes a API

--- processarBairrosPorCep.php
+++ processarBairrosPorCep.php
@@ -1,44 +1,45 @@
-    <?php
-    set_time_limit(0);
-    $f_in = fopen('bairros-carnaval-2026.csv', 'r');
-    $f_out = fopen('BairroPorCep.csv', 'w');
+<?php
+set_time_limit(0);
 
-    fwrite($f_out, "\xEF\xBB\xBF");
-    fputcsv($f_out, ['Bairro', 'UF', 'CEP'], ";");
+$f_in = fopen('ListaCepsCarnaval.csv', 'r'); 
+$f_out = fopen('BairroPorCep.csv', 'w');
 
-    $ch = curl_init();
-    curl_setopt_array($ch, [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_USERAGENT      => 'Mozilla/5.0'
-    ]);
+fwrite($f_out, "\xEF\xBB\xBF");
+fputcsv($f_out, ['Bairro_API', 'CEP'], ";"); // Cabeçalho sem UF
 
-    echo "Processando...\n";
+$ch = curl_init();
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_USERAGENT      => 'Mozilla/5.0'
+]);
 
-    while ($linha = fgetcsv($f_in)) {
-        $cep = preg_replace('/[^0-9]/', '', $linha[0]);
-        if (strlen($cep) !== 8) continue;
+$cache_ceps = []; 
 
+echo "Processando...\n";
+
+while ($linha = fgetcsv($f_in, 0, ";")) {
+    $cep = preg_replace('/[^0-9]/', '', $linha[0]);
+    if (strlen($cep) !== 8) continue;
+
+    // Se NÃO estiver no cache, busca na API
+    if (!isset($cache_ceps[$cep])) {
         curl_setopt($ch, CURLOPT_URL, "https://viacep.com.br/ws/$cep/json/");
         $res = json_decode(curl_exec($ch), true);
 
         if (isset($res['bairro'])) {
-            
-            if (!empty($res['bairro'])) {
-                $nome = $res['bairro'];
-            } else {
-                $nome = $res['localidade'];
-            }
-
-            $nome = trim(explode(',', preg_replace('/\s?\(.*?\)/', '', $nome))[0]);
-
-            fputcsv($f_out, [$nome, $res['uf'], $cep], ";");
-            echo "$cep -> $nome\n";
+            $nome = !empty($res['bairro']) ? $res['bairro'] : $res['localidade'];
+            $cache_ceps[$cep] = trim(explode(',', preg_replace('/\s?\(.*?\)/', '', $nome))[0]);
+            echo "API: $cep -> {$cache_ceps[$cep]}\n";
+            sleep(1); 
+        } else {
+            $cache_ceps[$cep] = 'NÃO ENCONTRADO';
         }
-        usleep(500000); 
     }
 
-    fclose($f_in);
-    fclose($f_out);
-    curl_close($ch);
-    echo "Finalizado!";
+    // Grava no CSV o que estiver no cache (seja o bairro ou o erro)
+    fputcsv($f_out, [$cache_ceps[$cep], $cep], ";");
+}
+
+fclose($f_in); fclose($f_out); curl_close($ch);
+echo "Finalizado!";
